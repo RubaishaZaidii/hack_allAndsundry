89560c0dbd0fba5764713ff0e199dcc66c20feac
Rubaisha Zaidi <bsef21m022@pucit.edu.pk>
2023-10-20 22:00:43 +0500

    Add files via upload

---
+++ palindromestring.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+bool isPalindrome(string *s,string *x)
+{
+        
+}
+int main()
+{
+    string *s="abba",*x;
+    for (int i = 0; i < s.length(); i++)
+    {
+        x+=s[i];
+    }
+    cout<<"it is palindrome"<<isPalindrome(s,x);
+    return 0;
+}
